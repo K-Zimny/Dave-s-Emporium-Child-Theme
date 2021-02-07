01fc06be6d2b965c274e61850a2f7dcb69562633
K-Zimny <kenneth.zimny@gmail.com>
2021-02-07 15:05:52 -0600

    Restored to base file

--- front-page.php
+++ front-page.php
@@ -41,37 +41,66 @@ get_header(); ?>
 						echo $queried_post->post_content;
 					?>
 				</div>
-				<?php echo do_shortcode('[metaslider id="47"]'); ?>
 			</div>
 		</section>
 
 		<section class="shop">
 			<div class="content-wrapper">
-				<?php
-					$post_id = 16;
-					$queried_post = get_post($post_id);
-					echo $queried_post->post_content;
-				?>
+				<div class="shop-content">
+					<?php
+						$post_id = 16;
+						$queried_post = get_post($post_id);
+						echo $queried_post->post_content;
+					?>
+					<div class="content-button">
+						<button class="button" type="button" onclick="location.href='#contact';">SHOP NOW</button>
+					</div>
+				</div>
+				<figure>
+					<div class="shop-img">
+						<img src="https://images.unsplash.com/photo-1508748169069-82590c9f26e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1267&q=80" />
+					</div>
+				</figure>
 			</div>
 		</section>
 
 		<section class="education">
 			<div class="content-wrapper">
-				<?php
-					$post_id = 18;
-					$queried_post = get_post($post_id);
-					echo $queried_post->post_content;
-				?>
+				<div class="education-content">
+					<?php
+						$post_id = 18;
+						$queried_post = get_post($post_id);
+						echo $queried_post->post_content;
+					?>
+					<div class="content-button">
+						<button class="button" type="button" onclick="location.href='#contact';">Learn more</button>
+					</div>
+				</div>
+				<figure>
+					<div class="education-img">
+						<img src="https://images.unsplash.com/photo-1599008633840-052c7f756385?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" />
+					</div>
+				</figure>
 			</div>
 		</section>
 
 		<section class="about">
 			<div class="content-wrapper">
-				<?php
-					$post_id = 21;
-					$queried_post = get_post($post_id);
-					echo $queried_post->post_content;
-				?>
+				<div class="about-content">
+					<?php
+						$post_id = 21;
+						$queried_post = get_post($post_id);
+						echo $queried_post->post_content;
+						?>
+					<div class="content-button">
+						<button class="button" type="button" onclick="location.href='#contact';">learn more</button>
+					</div>
+				</div>
+				<figure>
+					<div class="about-img">
+						<img src="https://images.unsplash.com/photo-1517048676732-d65bc937f952?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" />
+					</div>
+				</figure>
 			</div>
 		</section>
 
