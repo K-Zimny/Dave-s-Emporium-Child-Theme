2490633dba86a53637d2dcff4eee651af2382dad
K-Zimny <kenneth.zimny@gmail.com>
2021-02-08 12:17:10 -0600

    Added Daves paragraph

--- about-template.php
+++ about-template.php
@@ -43,6 +43,25 @@ get_header();
 				</div>
 			</section>
 
+			<section class="team">
+				<div class="content-wrapper">
+					<div class="bio">
+						<p>In my mental health journey I found cannabis to be a help, but after chasing issues with increased anxiety I was having when consuming cannabis, I decided to take a break, which eventually stretched to several years without regular use. It was during this time that many of my hobbies evolved and became more refined. I spent more time learning about coffee, food, equipment, 3D design, 3D printing, electronics and coding.</p>
+
+						<p>In early 2019, I was diagnosed with Rheumatoid Arthritis and again, cannabis was there and presented a safe way to relieve myself from the chronic pain and day to day anxiety of having an autoimmune disease. The recent legality of medical cannabis in my home state of Illinois made it a much more viable option for me. After becoming very familiar with my local dispensary, I had a realization: most of my issues with cannabis in the past were due to it being poorly quality and not handled with the care of the medicine that it is.</p>
+
+						<p>Most recently, I can thank cannabis for keeping my sane during the pandemic. Between my own health issues and my partners age, leaving the house has become less and less frequent as the pandemic drags on. Growing cannabis has always been an interest of mine. I dabbled a bit back when I was in my teens, but the technology and the costs weren’t quite accessible to allow for good, discreet home grows for a kid on a budget.</p>
+
+						<p>In January of 2020, as the laws changed, I started my first legal home grow as a medical patient. I have now grown many well known strains including White Widow, Blueberry Kush, Bruce Banner, Chem Dog #4, as well as dabbling with offerings from many micro breeders from around the country. I want to thank Cyndi’s Seeds and Prairie State Genetics, just to name a couple, who have been releasing unique offerings with their own twists and adding a bit of fun to our industry.</p>
+
+						<p>Breeding cannabis has always interested me, but before the pandemic, I was never in one place long enough to consider cultivation of more than a couple plants, let alone thinking about several grow cycles at once (ahhhh, the life of consulting and project management). So, lets mark this as one of the few items in the “Pros” column 2020.</p>
+
+						<p>I have now begun the breeding process, experimenting with my favorite strains and harvesting and preserving pollen from traditional male plants as well as those who have had their sex reversed to produce female pollen. My first major pheno hunt is going on now for our inaugural cross, Medicine Man C5 from Cyndis Seeds x White Widow (Fem Pollen/CS Reversed) from Growers Choice. Eventually, the goal is for this to evolve into “QueerBud”, a nod to being LGBT in a world riddled with folks looking to put us down. Testers are available for sale now as well as an F2 of the White Widow mother used in this cross.</p>
+
+						<p>So this brings us to today! Welcome to Dave’s Emporium. I am excited for what’s ahead. Thank you for following us along on our journey!</p>
+					</div>
+				</div>
+			</section>
 			
 		</main><!-- #main -->
 	</div><!-- #primary -->
