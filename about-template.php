2e92decf92cc99aee884d56036cfa435a2822be6
K-Zimny <kenneth.zimny@gmail.com>
2021-02-08 12:10:52 -0600

    about-page text edits

--- about-template.php
+++ about-template.php
@@ -43,22 +43,7 @@ get_header();
 				</div>
 			</section>
 
-			<?php
-
-			// Start the Loop.
-			while ( have_posts() ) :
-				the_post();
-
-				get_template_part( 'template-parts/content/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-
-			endwhile; // End the loop.
-			?>
-
+			
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
