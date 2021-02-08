7f2585d581b2c18f3880b28a028776c59991c29d
K-Zimny <kenneth.zimny@gmail.com>
2021-02-08 12:27:34 -0600

    Added Ginny's paragraph

--- about-template.php
+++ about-template.php
@@ -46,7 +46,7 @@ get_header();
 			<section class="team">
 				<div class="content-wrapper">
 					<div class="bio">
-						<p>In my mental health journey I found cannabis to be a help, but after chasing issues with increased anxiety I was having when consuming cannabis, I decided to take a break, which eventually stretched to several years without regular use. It was during this time that many of my hobbies evolved and became more refined. I spent more time learning about coffee, food, equipment, 3D design, 3D printing, electronics and coding.</p>
+						<p><img class="bio-img" src="http://daves-emporium.local/wp-content/uploads/2021/02/DaveProfPic-scaled-1-768x1024.jpeg"/>In my mental health journey I found cannabis to be a help, but after chasing issues with increased anxiety I was having when consuming cannabis, I decided to take a break, which eventually stretched to several years without regular use. It was during this time that many of my hobbies evolved and became more refined. I spent more time learning about coffee, food, equipment, 3D design, 3D printing, electronics and coding.</p>
 
 						<p>In early 2019, I was diagnosed with Rheumatoid Arthritis and again, cannabis was there and presented a safe way to relieve myself from the chronic pain and day to day anxiety of having an autoimmune disease. The recent legality of medical cannabis in my home state of Illinois made it a much more viable option for me. After becoming very familiar with my local dispensary, I had a realization: most of my issues with cannabis in the past were due to it being poorly quality and not handled with the care of the medicine that it is.</p>
 
@@ -59,6 +59,15 @@ get_header();
 						<p>I have now begun the breeding process, experimenting with my favorite strains and harvesting and preserving pollen from traditional male plants as well as those who have had their sex reversed to produce female pollen. My first major pheno hunt is going on now for our inaugural cross, Medicine Man C5 from Cyndis Seeds x White Widow (Fem Pollen/CS Reversed) from Growers Choice. Eventually, the goal is for this to evolve into “QueerBud”, a nod to being LGBT in a world riddled with folks looking to put us down. Testers are available for sale now as well as an F2 of the White Widow mother used in this cross.</p>
 
 						<p>So this brings us to today! Welcome to Dave’s Emporium. I am excited for what’s ahead. Thank you for following us along on our journey!</p>
+					</div>
+					<div class="bio">
+					<p>The first time I smoked medical marijuana, I remember feeling the release of tension in my spine for the first time. When I began smoking, it was shitty brick weed as a pre-teen. When I found medicinal cannabis, it was a game changer. All of a sudden, I knew what I was signing up for. That was a radical moment when I was able to pick and choose what strains I needed and what kind of high I wanted. It also allows you to find out what works for other people. That’s really when it became medicine for me.</p>
+
+					<p>After I found medicinal marijuana, I realized I could use it to treat mental health issues and pain like nothing else had before. I’ve suffered from chronic pain and anxiety for 29 years, being diagnosed with severe scoliosis in my early childhood. I never wanted to entertain pharmaceutical drugs because I can’t stand behind the damage it’s done and I didn’t want that for myself. Finding medical cannabis changed my life.</p>
+
+					<p>Now that things have started to legalize, it’s really amazing to be a part of something that people can do at home for themselves. White Widow has given me the most relief medically, releasing me from daily pain. It also relieves anxiety and that’s why it’s been one of our focal strains since the beginning. We use it in our famous QueerBud.</p>
+
+					<p>It’s insanely therapeutic to nurture something to its full potential and make products that genetically produce the best plants, giving you the best results. I love educating people on weed: how to grow, how to smoke it, and how to pick and choose what’s best for you. It’s a really magical plant that helps a lot of people. I look forward to seeing the way it moves across the country and across the world.<p>
 					</div>
 				</div>
 			</section>
