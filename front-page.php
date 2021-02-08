1d866685c0d2f1bc7e9d9f4db347d341173185c9
K-Zimny <kenneth.zimny@gmail.com>
2021-02-08 12:08:49 -0600

    Nav styles for mobile page and about nav

--- front-page.php
+++ front-page.php
@@ -16,7 +16,7 @@ get_header(); ?>
 			<div class="content-wrapper">
 				<figure>
 					<div class="header-img">
-						<img src="https://i1.wp.com/davesemporium.xyz/wp-content/uploads/2020/12/DE_LOGO.png?w=1920&ssl=1" />
+						<a href="http://daves-emporium.local"> <img src="https://i1.wp.com/davesemporium.xyz/wp-content/uploads/2020/12/DE_LOGO.png?w=1920&ssl=1" /></a>
 					</div>
 				</figure>
 				<div class="header-title">
@@ -24,9 +24,21 @@ get_header(); ?>
 				</div>
 				<nav>
 					<ul>
-						<li>SHOP</li>
-						<li>EDUCATION</li>
-						<li>ABOUT</li>
+						<li>
+							<a style="color:black;" href="http://daves-emporium.local/shop/">SHOP</a>
+						</li>
+						<li>
+							<a style="color:black;" href="http://daves-emporium.local/education/">EDUCATION</a>						
+						</li>
+						<li>
+							<a style="color:black;" href="http://daves-emporium.local/our-story/">ABOUT</a>
+						</li>
+						<li>
+							<a style="color:black;" href="http://daves-emporium.local/">COMMUNITY</a>
+						</li>
+						<li>
+							<a style="color:black;" href="http://daves-emporium.local/">SOCIAL</a>						
+						</li>
 					</ul>
 				</nav>
 			</div>
